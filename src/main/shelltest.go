171ce3af26354fba25eee0688d1b17522ce2ba44
thevone119 <57717235@qq.com>
2020-02-09 09:15:51 +0800

    123

---
+++ src/main/shelltest.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main(){
+	reader:=bufio.NewReader(os.Stdin)
+	for{
+		fmt.Print("$")
+		cmdString,err:=reader.ReadString('\n')
+		if err!=nil{
+			fmt.Fprintln(os.Stderr,err)
+		}
+		runCommand(cmdString)
+	}
+}
+
+func runCommand(commandStr string) error{
+	commandStr=strings.TrimSuffix(commandStr,"\n")
+	arrcmd:=strings.Fields(commandStr)
+	switch arrcmd[0] {
+	case "syncup":
+		fmt.Fprintln(os.Stdout,"syncup")
+	case "testsync":
+		fmt.Fprintln(os.Stdout,"testsync")
+	}
+	return nil
+}
