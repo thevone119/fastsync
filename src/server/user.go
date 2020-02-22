05ce96ed2be78f79bf11834d80c7d9db4a2c8bf7
thevone119 <57717235@qq.com>
2020-02-23 06:40:15 +0800

    123

--- src/server/user.go
+++ src/server/user.go
@@ -5,26 +5,26 @@ import (
 	"zinx/ziface"
 )
 
-//连接用户对象
+//连接用户对象,作废
 type User struct {
-	Uid  uint32              //用户的ID，sessionid
-	Conn ziface.IConnection //当前用户的连接
-	UserName string    //用户名
+	Uid      uint32             //用户的ID，sessionid
+	Conn     ziface.IConnection //当前用户的连接
+	UserName string             //用户名
 
 }
 
 //创建一个玩家对象
-func NewUser(usrName string,conn ziface.IConnection) *User {
+func NewUser(usrName string, conn ziface.IConnection) *User {
 	return &User{
-		Uid:utils.GetNextUint(),
-		 Conn:conn,
-		 UserName:usrName,
+		Uid:      utils.GetNextUint(),
+		Conn:     conn,
+		UserName: usrName,
 	}
 }
 
 //用户登录
-func (u *User) Login(userName string,pwd string) bool{
-	if (userName=="admin" &&pwd=="admin"){
+func (u *User) Login(userName string, pwd string) bool {
+	if userName == "admin" && pwd == "admin" {
 		return true
 	}
 	return false
