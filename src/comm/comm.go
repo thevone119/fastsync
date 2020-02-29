a32e699b7d9c7745172be004528e494ea1a37c8b
thevone119 <57717235@qq.com>
2020-02-29 09:06:55 +0800

    123

--- src/comm/comm.go
+++ src/comm/comm.go
@@ -1,6 +1,8 @@
 package comm
 
 import (
+	"bytes"
+	"encoding/binary"
 	"os"
 	"strings"
 )
@@ -43,4 +45,28 @@ func AppendPath(p1 string, p2 string) string {
 		p2 = p2[1:]
 	}
 	return p1 + "/" + p2
+}
+
+//整形转换成字节
+func IntToBytes(n int) []byte {
+	x := int32(n)
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+func UIntToBytes(n uint32) []byte {
+	x := int32(n)
+	bytesBuffer := bytes.NewBuffer([]byte{})
+	binary.Write(bytesBuffer, binary.BigEndian, x)
+	return bytesBuffer.Bytes()
+}
+
+//字节转换成整形
+func BytesToInt(b []byte) int {
+	bytesBuffer := bytes.NewBuffer(b)
+
+	var x int32
+	binary.Read(bytesBuffer, binary.BigEndian, &x)
+
+	return int(x)
 }
---
+++ src/comm/db.go
@@ -0,0 +1,125 @@
+package comm
+
+import (
+	"bytes"
+	"encoding/binary"
+	"github.com/boltdb/bolt"
+	"time"
+	"zinx/zlog"
+)
+
+//用bolt做的简单的key/value存储
+
+//全局变量，直接使用
+var FileDB *filedb
+
+type filedb struct {
+	path   string
+	db     *bolt.DB
+	isopen bool
+}
+
+/*
+	提供init方法，默认加载
+*/
+func init() {
+	//初始化全局变量，设置一些默认值
+	FileDB = &filedb{
+		path: "fastsync.db",
+	}
+	FileDB.open()
+}
+
+func (f *filedb) open() {
+	if f.isopen {
+		return
+	}
+	db, err := bolt.Open(f.path, 0600, &bolt.Options{Timeout: 5 * time.Second})
+	if err != nil {
+		zlog.Error("open db err", err)
+		return
+	}
+	f.db = db
+	f.isopen = true
+}
+
+func (f *filedb) PutString(bucket string, key string, value string) error {
+	err := f.db.Update(func(tx *bolt.Tx) error {
+		// 创建一个桶
+		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			zlog.Error("filedb PutString err", err)
+			return err
+		}
+		err = b.Put([]byte(key), []byte(value))
+		if err != nil {
+			zlog.Error("filedb PutString2 err", err)
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func (f *filedb) PutInt64(bucket string, key string, value int64) error {
+	err := f.db.Update(func(tx *bolt.Tx) error {
+		// 创建一个桶
+		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			zlog.Error("filedb PutString err", err)
+			return err
+		}
+		bytesBuffer := bytes.NewBuffer([]byte{})
+		binary.Write(bytesBuffer, binary.BigEndian, value)
+		err = b.Put([]byte(key), bytesBuffer.Bytes())
+		if err != nil {
+			zlog.Error("filedb PutString2 err", err)
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func (f *filedb) GetString(bucket string, key string) (string, error) {
+	var ret = ""
+	err := f.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		if b == nil {
+			return nil
+		}
+		v := b.Get([]byte(key))
+		ret = string(v[:])
+		return nil
+	})
+	return ret, err
+}
+
+func (f *filedb) GetInt64(bucket string, key string) (int64, error) {
+	var ret = int64(-1)
+	err := f.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		if b == nil {
+			return nil
+		}
+		v := b.Get([]byte(key))
+		bytesBuffer := bytes.NewBuffer(v)
+		var x int64
+		binary.Read(bytesBuffer, binary.BigEndian, &x)
+		ret = x
+		return nil
+	})
+	return int64(-1), err
+}
+
+func (f *filedb) RemoveString(bucket string, key string) error {
+	err := f.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucket))
+		if b == nil {
+			return nil
+		}
+		b.Delete([]byte(key))
+		return nil
+	})
+	return err
+}
